f06bd891169c964200b7a3b5898e7c447c46301a
WalissonCM <walissoncavalcante@outlook.com>
2023-06-19 21:54:22 -0300

    projeto

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,10 +1,14 @@
-describe('Test', () => {
-  it('Requerimento', () => {
-    cy.visit('http://localhost:3000/requerimentos')
-    cy.get('.btn').click() 
-    cy.get('#inputDisciplina').select('ADS030 - Manutenção de Software e DevOps')
-    cy.get('#inputProva').select('A1')
-    cy.get('#inputArgumentacao').type('Test')
-    cy.get('.btn-danger').click()
+describe('Logout', () => {
+  beforeEach(() => {
+    cy.login('https://localhost:3000/login') 
+  })
+
+  it('logout redireciona para à página de login', () => {
+    cy.contains('Logout')
+      .should('be.visible')
+      .click('https://localhost:3000/logout')
+
+    cy.url('https://localhost:3000/')
+      .should('be.equal', 'https://localhost:3000/login')
   })
 })
